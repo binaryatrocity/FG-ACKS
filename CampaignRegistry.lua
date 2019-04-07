36b261bcd0184678e45fd41e231b19766634b1e1
binaryatrocity <brandon.cornejo@gmail.com>
2019-04-07 18:44:13 -0500

    Initial commit

---
+++ CampaignRegistry.lua
@@ -0,0 +1,9 @@
+{
+	["OptREVL"] = "on",
+	["OptINIT"] = "off",
+	["OptRSHE"] = "on",
+	["OptCTSH"] = "off",
+	["OptRING"] = "on",
+	["OptBARC"] = "",
+	["sidebar"] = "charsheet,note,image,table,story,quest,npc,battle,item,treasureparcel",
+}
